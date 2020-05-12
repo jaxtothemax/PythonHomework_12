dbbd018e3ce549a9706e9950fa01b04df070d049
jaxtothemax <jax.atlet@hotmail.com>
2020-05-12 14:28:40 +0200

    removed useless code

--- [BONUS] - 12.3 - Geography_Game/geography_game.py
+++ [BONUS] - 12.3 - Geography_Game/geography_game.py
@@ -1,20 +1,20 @@
 import random
 
 def play_game():
-    country = {"Slovenia":"Ljubljana", "Croatia":"Zagreb", "Serbia":"Belgrade", "Spain":"Barcelona", 
+    country = {"Slovenia":"Ljubljana", "Croatia":"Zagreb", "Serbia":"Belgrade", "Spain":"Madrid", 
                "Austria":"Vienna", "Hungary":"Budapest", "Italy":"Rome", "UK":"London", 
-               "Slovakia":"Bratislava", "Germany":"Berlin", "Portugal":"Lisboa"}
+               "Slovakia":"Bratislava", "Germany":"Berlin", "Portugal":"Lisboa", "Norway":"Oslo", 
+               "Sweden":"Stockholm", "Denmark":"Copenhagen", "Russia":"Moscow", "Japan":"Tokyo", "Finland":"Helsinki",
+               "China":"Beijing", "India":"New Delhi"}
                
     random_country = random.choice(list(country))
-    capital_city = input(f"What is the capital city of {random_country}: ")
+    capital_city = input(f"What is the capital city of {random_country}: ").title()
 
-    key,value = random_country,capital_city
-    if key in country and value == country[key]:
+    if capital_city == country[random_country]:
         print("That's correct, continue!")
     else:
         print("That's incorrect, google it!")
 
     play_game()
   
-            
 play_game()
